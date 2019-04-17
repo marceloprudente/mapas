610a1e53712cb41bb008a8787cd7e558cff53616
Marcelo Prudente <33759575+marceloprudente@users.noreply.github.com>
2019-04-17 17:18:28 -0300

    Add files via upload
    Cria mapas do Brasil por meio de shapefiles obtidos no IBGE

---
+++ mapas_com_highchart_a_partir_de_shapefile.R
@@ -0,0 +1,59 @@
+# Criar highchart map a partir de shapefiles
+
+# baixar bibliotecas
+library(rgdal)
+library(spdplyr)
+library(geojsonio)
+library(rmapshaper)
+# baixar shapefile
+county <- readOGR(dsn = "Z:/CGAPP/Equipe/Capacitação/Base de dados/mapas", layer = "municipios_2010", verbose = FALSE)
+
+# manipulando bases com spdplyr
+county_id =  county %>% mutate(id2 = as.character(id))
+
+# transformar em json
+# no hichchart, só funciona se for uma lista
+mun_list <- geojsonio::geojson_list(county_id)
+
+# depois, vamos recuperar os dados do shapefile e fazer algumas transformações
+pop <- as.data.frame(county) %>% 
+  mutate_all(as.character) %>% 
+  mutate_at(vars(populacao, pib), as.numeric) %>% 
+  mutate(pop_faixas = cut(populacao,c(0, 15000, 50000, 100000, 1000000, Inf),
+                          labels = c("Até 15k", "Entre 15k e 50k",
+                                    "Entre 50k e 100k", "Entre 100k e 1m",
+                                    "Acima de 1m")))
+
+# dar uma olhada nos dados
+glimpse(pop)
+
+
+# Mapa  
+library(highcharter)
+highchart() %>% 
+  hc_add_series_map(mun_list,
+                pop,
+                joinBy =c("id2",  "id"),
+                value = "populacao",
+                name = "População",
+                borderColor = "red",
+                borderWidth = 0.2) %>% 
+  hc_title(text = "Brasil - população municipal") %>%
+  hc_colorAxis(dataClasses = color_classes(c(seq(800, 11253503, by = 1000000)))) %>% 
+  hc_legend(layout = "vertical", align = "right", valueDecimals = 2) 
+  
+
+highchart() %>% 
+  hc_add_series_map(mun_list,
+                    pop,
+                    joinBy =c("id2",  "id"),
+                    value = "populacao",
+                    name = "População",
+                    borderColor = "#FAFAFA",
+                    borderWidth = 0.1) %>% 
+  hc_title(text = "Brasil - população municipal") %>%
+  hc_colorAxis(dataClasses = color_classes(c(0, 15000, 50000, 100000, 1000000, Inf))) %>% 
+  hc_legend(layout = "vertical", align = "right", valueDecimals = 0) 
+
+  
+
