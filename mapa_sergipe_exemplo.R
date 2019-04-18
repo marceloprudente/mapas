36b37b841aea4c64f7639ec850873548e732087b
Marcelo Augusto Prudente Lima <marceloapl@sof.intra>
2019-04-18 10:57:44 -0300

    DS_NOM

--- mapa_sergipe_exemplo.R
+++ mapa_sergipe_exemplo.R
@@ -33,11 +33,15 @@ se <- as.data.frame(se_map) %>%
                           labels = c("Até 15k", "Entre 15k e 50k",
                                      "Entre 50k e 100k", "Entre 100k e 1m",
                                      "Acima de 1m")))
+# baixar chave dos municipios
 chave <- readxl::read_excel("~/dados/chave_municipios.xlsx") %>% 
   mutate(CO_MNICDV = as.character(CO_MNICDV))
 
+# mesclar tabelas
 se <- inner_join(se, chave, by = c("codigo_ibg" = "CO_MNICDV"))
 
+
+## MAPA 1 - SERGIPE - com rotulos municipios
 highchart() %>% 
   hc_add_series_map(se_list,
                     se,
@@ -48,8 +52,8 @@ highchart() %>%
                     borderWidth = 0.1,
                     # para identificar os rotulos, temos que ver as propriedades
                     # da lista: se_list$features[[1]]$properties
-                    # no caso dos dados do mapa, variavel chama-se nome
-                    # Por isso, especificamos point.nome
+                    # no caso dos dados do mapa, variavel chama-se DS_NOM
+                    # Por isso, especificamos point.DS_NOM
                     dataLabels = list(enabled = TRUE, format = '{point.DS_NOM}'),
                     # a mesma coisa para os labels interativos, alteramos o
                     # point.format
@@ -59,7 +63,7 @@ highchart() %>%
   hc_colorAxis(dataClasses = color_classes(c(0, 15000, 50000, 100000, 500000, 1000000))) %>% 
   hc_legend(layout = "vertical", align = "right", valueDecimals = 0) 
 
-# mapa sem títulos
+# MAPA 2 - SERGIPE sem títulos
 
 # cores do mapa
 cols <- ggsci::pal_startrek()(6) 
@@ -80,6 +84,5 @@ highchart() %>%
   hc_colorAxis(  minColor = "#BC3C29", maxColor = "#434348",
                  dataClasses = color_classes(breaks = c(0, 15000, 50000, 100000, 500000, 1000000), 
                                              colors = cols)) %>% 
-  hc_yAxis()
-hc_legend(layout = "vertical", align = "right", valueDecimals = 0) 
+  hc_legend(layout = "vertical", align = "right", valueDecimals = 0) 
 
